112923f579adb2839f83a6c8eba9307a053ece84
raigto <raigto@gmail.com>
2015-07-06 17:44:51 -0430

    Inicial

---
+++ springapp/src/main/java/com/companyname/springapp/service/SimpleProductManager.java
@@ -0,0 +1,44 @@
+package com.companyname.springapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.companyname.springapp.domain.Product;
+import com.companyname.springapp.repository.ProductDao;
+
+@Component
+public class SimpleProductManager implements ProductManager {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Autowired
+	private ProductDao productDao;
+
+	public void setProductDao(ProductDao productDao) {
+		this.productDao = productDao;
+	}
+
+	public void increasePrice(int percentage) {
+		List<Product> products = productDao.getProductList();
+		if (products != null) {
+			for (Product product : products) {
+				double newPrice = product.getPrice().doubleValue()
+						* (100 + percentage) / 100;
+				product.setPrice(newPrice);
+			}
+		}
+	}
+
+	public List<Product> getProducts() {
+		return productDao.getProductList();
+	}
+
+	// public void setProducts(List<Product> products) {
+	// this.products = products;
+	// }
+
+}
---
+++ springapp/src/test/java/com/companyname/springapp/domain/ProductTests.java
@@ -0,0 +1,32 @@
+package com.companyname.springapp.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProductTests {
+	private Product product;
+	
+	@Before
+	public void setUp() throws Exception{
+		product = new Product();
+	}
+
+	@Test
+	public void testSetAndGetterDescription() {
+		String testDescription = "descripcion";
+		assertNull(product.getDescription());
+		product.setDescription(testDescription);
+		assertEquals(testDescription, product.getDescription());
+	}
+	
+	@Test
+	public void testSetAndGetterPrice(){
+		Double testprice = 100.99;
+		assertNull(product.getPrice());
+		product.setPrice(testprice);
+		assertEquals(testprice, product.getPrice());
+	}
+
+}
